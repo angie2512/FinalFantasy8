c5178009328c2716975c0271fc258e856d4070ad
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2022-11-06 17:15:56 -0500

    hechizo borrar

--- src/main/java/com/example/finalfantasy/Servlet/HechizosServlet.java
+++ src/main/java/com/example/finalfantasy/Servlet/HechizosServlet.java
@@ -25,6 +25,15 @@ public class HechizosServlet extends HttpServlet {
                 try {
                     int spelli = Integer.parseInt(spell);
                     hechi.eliminarHechizo(spelli);
+                    ArrayList<Hechizos> listaHechizos = null;
+                    try {
+                        listaHechizos = hechi.listarHechizos();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    request.setAttribute("listaHechizos",listaHechizos);
+                    view = request.getRequestDispatcher("/menuhechizos.jsp");
+                    view.forward(request,response);
                 }catch (NumberFormatException e){
                     response.sendRedirect(request.getContextPath()+ "/HechizosServlet");
                 }
